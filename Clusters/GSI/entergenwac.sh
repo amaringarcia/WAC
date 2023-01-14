0d1e20cd0ff4e83c876d38f92f3d5e70ac3fe467
Victor <victor.gonzalez@cern.ch>
2023-01-14 23:55:47 +0100

    Adapting the SW production environment to the singularity scenario

--- Clusters/GSI/entergenwac.sh
+++ Clusters/GSI/entergenwac.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export ALIEN_SITE=GSI
-source /cvmfs/alice.cern.ch/etc/login.sh
-eval $(alienv printenv VO_ALICE@Python::v3.9.12-10)
-eval $(alienv printenv VO_ALICE@AliGenerators::v20221125-1)
-eval $(alienv printenv VO_ALICE@ROOT::v6-26-04-patches-alice2-22)
-eval $(alienv printenv VO_ALICE@CMake::v3.23.1-12)
+source <( /cvmfs/alice.cern.ch/bin/alienv printenv VO_ALICE@Python::v3.9.12-10)
+source <( /cvmfs/alice.cern.ch/bin/alienv printenv VO_ALICE@AliGenerators::v20221125-1)
+source <( /cvmfs/alice.cern.ch/bin/alienv printenv VO_ALICE@ROOT::v6-26-04-patches-alice2-22)
+source <( /cvmfs/alice.cern.ch/bin/alienv printenv VO_ALICE@CMake::v3.23.1-12)
 source set-WAC-GSI
 
